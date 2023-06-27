eaddfcb06d2d87ca9a03ca4e24ac5454130b2b87
SergioToroM <126355149+SergioToroM@users.noreply.github.com>
2023-06-26 22:34:12 -0500

    carrito

--- models/cliente.js
+++ models/cliente.js
@@ -3,9 +3,7 @@ const mongoose = require('../config/Database');
 const ClientesSchema = new mongoose.Schema({
     Nombre: {type: String, required: true},
     Telefono: {type: String, required: true},
-    Ubicacion: {centro: {type: Array}, Zoom:{type: Number}},
-    TotalCompras: {type: Number},
-    HistoricoCompras: {type: Number}
+    TotalCompras: {type: Number}
 });
 
 const cliente = mongoose.model('Cliente', ClientesSchema);
