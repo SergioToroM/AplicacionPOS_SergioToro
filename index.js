11cfceb95a861fe6cc312f315e5fa3a046702d16
SergioToroM <126355149+SergioToroM@users.noreply.github.com>
2023-08-12 11:32:35 -0500

    rutas

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const app = express();
 const path = require('path');
 
 
-const mirouter = require('./routes/enrutamiento')
+const mirouter = require('./backend/routes/enrutamiento')
 const env = require('dotenv');
-const { clientes } = require('./controllers/controller');
+const { clientes } = require('./backend/controllers/controller');
 env.config();
 app.use(logger('dev'));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './frontend/views'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/api',mirouter);
